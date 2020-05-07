04ae881953f92ffbfce4cd60c5e4b24e9507dc1d
Mai Rodrigues <maiarrod@gmail.com>
2020-05-07 20:06:17 -0300

    primeiro commit :D

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+
+fun main() {
+    println("Bem vindo ao ByteBank")
+    val titular: String = "Maiara"
+    val numeroConta = 10; //é um Int
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo $saldo")
+}
+
+/*
+    var - variável pode mudar
+    val - não pode mudar
+
+    É estaticamente tipada
+
+ */
